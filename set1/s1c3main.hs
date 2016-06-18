98f8e30fc387826aaed4cf6eb88d0c507909c8b0
Joseph Daniel Winters <jdwinters96@gmail.com>
2016-06-17 20:43:58 -0400

    Finished solution to S1C3

---
+++ set1/s1c3main.hs
@@ -0,0 +1,26 @@
+----------------------------------------
+-- Joseph Winters
+-- Cryptopals Set 1 Challenge 1 Program
+-- Spring 2016
+----------------------------------------
+
+
+import Set1Challenge3
+
+
+-- TODO: write main
+
+
+main = do
+  let x = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+      (score, ch, dec) = crackSingleCharXOR x
+  putStrLn "Given the hex string:"
+  putStrLn ("\"" ++ x ++ "\"")
+  putStrLn "the decryption by single character XOR is:"
+  putStrLn ("\"" ++ dec ++ "\"")
+  putStrLn ""
+  putStr "This decryption received a score of "
+  putStr . show $ score
+  putStr " (lower is better) and\nwas carried out with the character "
+  putStr . show $ ch
+  putStrLn "."
