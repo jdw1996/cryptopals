02880e2fad261b6e099dd289c173e8ccb9e4e3c6
Joseph Daniel Winters <jdwinters96@gmail.com>
2016-07-03 00:54:58 -0400

    Added Java solution to S1C4

---
+++ java/set1/S1C4Main.java
@@ -0,0 +1,28 @@
+/*
+ * Joseph Winters
+ * Cryptopals Set 1 Challenge 4 Program
+ * Spring 2016
+ */
+
+
+public class S1C4Main {
+
+    public static void main(String[] args) {
+        String filename = "S1C4Data.txt";
+        Set1Challenge3.XORDecryption result
+            = Set1Challenge4.findAndDecrypt(filename);
+
+        System.out.println("The hex string decrypted is:");
+        System.out.println("\"" + result.getCiphertext() + "\"");
+        System.out.println("and the decryption by single character XOR is:");
+        System.out.println("\"" + result.getPlaintext() + "\"");
+        System.out.println("");
+        System.out.println("This decryption received a score of "
+                           + result.getScore()
+                           + " (higher is better) and");
+        System.out.println("was carried out with the character '"
+                           + result.getKey()
+                           + "'.");
+    }
+
+}
---
+++ java/set1/Set1Challenge4.java
@@ -0,0 +1,44 @@
+/*
+ * Joseph Winters
+ * Cryptopals Set 1 Challenge 4 Functions
+ * Spring 2016
+ */
+
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import java.nio.charset.Charset;
+
+
+public class Set1Challenge4 {
+
+    // Return an XORDecryption representing the best decryption of any of the
+    //   hex strings in the file filename.
+    public static Set1Challenge3.XORDecryption findAndDecrypt(String filename) {
+        String currLine;
+        Set1Challenge3.XORDecryption bestXORDecryption = null;
+        Set1Challenge3.XORDecryption currXORDecryption;
+        try ( InputStream inStream = new FileInputStream(filename);
+              InputStreamReader inStreamReader
+                  = new InputStreamReader(inStream, Charset.forName("UTF-8"));
+              BufferedReader bufReader = new BufferedReader(inStreamReader) ) {
+            while (true) {
+                currLine = bufReader.readLine();
+                if (currLine == null) break;
+                currXORDecryption = Set1Challenge3.crackSingleCharXOR(currLine);
+                if (bestXORDecryption == null
+                    || currXORDecryption.getScore()
+                       > bestXORDecryption.getScore()) {
+                    bestXORDecryption = currXORDecryption;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Unable to access and read file " + filename);
+        }
+        return bestXORDecryption;
+    }
+
+}
