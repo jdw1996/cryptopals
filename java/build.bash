32e82bf2f0d99719c9d5be22f3513d6d33a049e5
Joseph Daniel Winters <jdwinters96@gmail.com>
2016-07-30 14:38:18 -0400

    Fixed error in Java build script

--- java/build.bash
+++ java/build.bash
@@ -2,7 +2,7 @@
 
 
 # Create build directory if it doesn't already exist
-mkdir build > /dev/null
+mkdir build 2> /dev/null
 
 # Compile all *.java files
 javac -sourcepath src/ -d build src/**/*.java
