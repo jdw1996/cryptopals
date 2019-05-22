e10c1269179bb70c97aaf036739e8bffad9338eb
Joseph Daniel Winters <jdwinters96@gmail.com>
2019-05-22 17:02:39 -0400

    Make base64 utility deal with bytes, not ASCII

--- c/src/util/base64.c
+++ c/src/util/base64.c
@@ -13,7 +13,7 @@
 /* HELPER FUNCTIONS */
 
 // Given a number in the range [0,63], return the corresponding base-64 digit.
-char b64_value_encode(char n) {
+char b64_value_encode(uint8_t n) {
 	if (0 <= n && n <= 25) {
 		return 'A' + n;
 	} else if (26 <= n && n <= 51) {
@@ -28,7 +28,7 @@ char b64_value_encode(char n) {
 }
 
 // Given a base-64 digit, return the corresponding numerical value.
-char b64_value_decode(char n) {
+uint8_t b64_value_decode(char n) {
 	if ('A' <= n && n <= 'Z') {
 		return n - 'A';
 	} else if ('a' <= n && n <= 'z') {
@@ -45,7 +45,7 @@ char b64_value_decode(char n) {
 /* EXPORTED FUNCTIONS */
 
 // See header file.
-char *b64_encode(const char *data) {
+char *b64_encode(const uint8_t *data) {
 	// Find the expected length of the encoded string, allocate memory for it.
 	int len = strlen(data);
 	const int encoded_len = 4 * (len / 3 + (len % 3 != 0));  // 4 * ceil(len / 3)
@@ -65,27 +65,27 @@ char *b64_encode(const char *data) {
 	for (int i = 0; i < num_iterations; i++) {
 		// Determine if this is the last iteration.
 		bool is_last_iteration = (i == num_iterations - 1);
-		// Retrieve the characters to encode.
-		char chunk_char0 = data[3 * i];
-		char chunk_char1 =
+		// Retrieve the bytes to encode.
+		uint8_t byte0 = data[3 * i];
+		uint8_t byte1 =
 			padding_needed == 2 && is_last_iteration ?
 			'\0' :
 			data[3 * i + 1];
-		char chunk_char2 =
+		uint8_t byte2 =
 			padding_needed >= 1 && is_last_iteration ?
 			'\0' :
 			data[3 * i + 2];
 		// Set the encoding characters.
-		ret[4 * i] = b64_value_encode(chunk_char0 >> 2);
-		ret[4 * i + 1] = b64_value_encode(((chunk_char0 & 0x03) << 4) | (chunk_char1 >> 4));
+		ret[4 * i] = b64_value_encode(byte0 >> 2);
+		ret[4 * i + 1] = b64_value_encode(((byte0 & 0x03) << 4) | (byte1 >> 4));
 		ret[4 * i + 2] =
 			padding_needed == 2 && is_last_iteration ?
 			'=' :
-			b64_value_encode(((chunk_char1 & 0x0f) << 2) | (chunk_char2 >> 6));
+			b64_value_encode(((byte1 & 0x0f) << 2) | (byte2 >> 6));
 		ret[4 * i + 3] =
 			padding_needed >= 1 && is_last_iteration ?
 			'=' :
-			b64_value_encode(chunk_char2 & 0x3f);
+			b64_value_encode(byte2 & 0x3f);
 	}
 
 	// Add null terminator.
@@ -95,35 +95,35 @@ char *b64_encode(const char *data) {
 }
 
 // See header file.
-char *b64_decode(const char *data) {
+uint8_t *b64_decode(const char *data) {
 	// Find the expected length of the decoded string, allocate memory for it.
 	int len = strlen(data);
 	int padding_used = (data[len - 2] == '=') + (data[len - 1] == '=');
 	const int decoded_len = 3 * len/4 - padding_used;
-	char *ret = malloc((decoded_len + 1) * sizeof(*ret));
+	uint8_t *ret = malloc((decoded_len + 1) * sizeof(*ret));
 	if (ret == NULL) return ret;
 
 	// Decode one chunk at a time.
 	for (int i = 0; i < len/4; i++) {
 		// Determine if this is the last iteration.
 		bool is_last_iteration = (i == len/4 - 1);
-		// Retrieve the characters to decode.
-		char chunk_char0 = b64_value_decode(data[4 * i]);
-		char chunk_char1 = b64_value_decode(data[4 * i + 1]);
-		char chunk_char2 =
+		// Retrieve the values to decode.
+		uint8_t value0 = b64_value_decode(data[4 * i]);
+		uint8_t value1 = b64_value_decode(data[4 * i + 1]);
+		uint8_t value2 =
 			padding_used == 2 && is_last_iteration ?
 			'\0' :
 			b64_value_decode(data[4 * i + 2]);
-		char chunk_char3 =
+		uint8_t value3 =
 			padding_used >= 1 && is_last_iteration ?
 			'\0' :
 			b64_value_decode(data[4 * i + 3]);
-		// Get the decoded characters.
-		ret[3 * i] = (chunk_char0 << 2) | (chunk_char1 >> 4);
+		// Get the decoded bytes.
+		ret[3 * i] = (value0 << 2) | (value1 >> 4);
 		if (!is_last_iteration || padding_used <= 1)
-			ret[3 * i + 1] = (chunk_char1 << 4) | (chunk_char2 >> 2);
+			ret[3 * i + 1] = (value1 << 4) | (value2 >> 2);
 		if (!is_last_iteration || padding_used == 0)
-			ret[3 * i + 2] = (chunk_char2 << 6) | chunk_char3;
+			ret[3 * i + 2] = (value2 << 6) | value3;
 	}
 
 	// Add null terminator.
@@ -133,6 +133,6 @@ char *b64_decode(const char *data) {
 }
 
 // See header file.
-void b64_free(char *data) {
+void b64_free(void *data) {
 	free(data);
 }
--- c/src/util/base64.h
+++ c/src/util/base64.h
@@ -7,10 +7,10 @@
 #include <stdint.h>
 
 // Return a base-64 encoded version of data.
-char *b64_encode(const char *data);
+char *b64_encode(const uint8_t *data);
 
 // Return a decoded version of the base-64 encoded string data.
-char *b64_decode(const char *data);
+uint8_t *b64_decode(const char *data);
 
 // Free the memory from an array returned by b64_encode or b64_decode.
-void b64_free(char *arr);
+void b64_free(void *arr);
